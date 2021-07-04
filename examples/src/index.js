9e28e303a1f94c86dfc46fb3ffb6eea23a673f6c
Timothy Foster <tf.auroratide@gmail.com>
2021-07-04 13:50:49 -0500

    Indicate toggling default transition with button styling

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,6 +2,21 @@ require('./_global')
 
 const popout = window.ImgPopoutElement.defaultTransitions.in
 
+const setActivated = (selector, name) => {
+    const elems = document.querySelectorAll(selector)
+    for (let elem of elems) {
+        elem.classList.remove('activated')
+    }
+
+    document.getElementById(name).classList.add('activated')
+}
+
+document.querySelector('#use-spin').onclick = () => {
+    setActivated('#custom-default-transitions button', 'use-spin')
+    useSpin()
+}
+
 document.querySelector('#use-popout').onclick = () => {
+    setActivated('#custom-default-transitions button', 'use-popout')
     ImgPopoutElement.defaultTransitions.in = popout
 }
